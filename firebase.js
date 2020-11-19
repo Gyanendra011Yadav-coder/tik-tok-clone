8afe13e231b4c125de40339bd6e79703c22bec27
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2020-11-19 22:09:32 +0530

    making a tik-tok-clone project

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBFfYU6SCQKH16DZu0MJqJW22QbXLtHgt0",
+    authDomain: "tik-tok-clone-1018.firebaseapp.com",
+    databaseURL: "https://tik-tok-clone-1018.firebaseio.com",
+    projectId: "tik-tok-clone-1018",
+    storageBucket: "tik-tok-clone-1018.appspot.com",
+    messagingSenderId: "1084511430553",
+    appId: "1:1084511430553:web:c9f2c10294f02753c15508",
+    measurementId: "G-03XW2W8VL9"
+  };
